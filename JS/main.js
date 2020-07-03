92dcefddbf1ca05759b06e8914b6e995d3eafd79
Julian Alfonso <juliandavid.alfonso@utp.edu.co>
2020-07-03 16:00:22 -0500

    Animación simple de contador regresivo con objeto de javascript

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,17 @@
-anime({
-    targets: '.css-selector-demo .el',
-    duration: 5000,
-    translateX: 500
-  });
+var logEL = document.querySelector('.battery-log');
+
+var battery = {
+  charged: '0%',
+  bueno: 0   
+}
+
+var animation = anime({
+  targets: battery,
+  charged: '100%',
+  bueno: 130,
+  round: 1,
+  easing: 'linear',
+  update: function(){
+    logEL.innerHTML = JSON.stringify(battery);
+  }
+});
